487f70195e9710a1f16480627848c4f792e6619f
Rafael Mercedes <rafaelmersant@gmail.com>
2019-12-29 00:50:48 -0400

    Update show payroll and send payroll

--- Controllers/AjaxController.cs
+++ Controllers/AjaxController.cs
@@ -12,10 +12,191 @@ namespace VolanteNominaRC.Controllers
 {
     public class AjaxController : Controller
     {
+
+        public struct Payroll
+        {
+            public string period;
+            public string year;
+            public string month;
+            public string fortnight;
+            public string description;
+            public string paytype;
+        }
+
+        public struct PayrollDetailRows
+        {
+            public string ceingdeduc;
+            public string cetipotrans;
+            public string cedesctran;
+            public decimal cevaltrans;
+        }
+
+        public struct PayrollDetailHeader
+        {
+            public string cecodire;
+            public string cetipopago;
+            public string cedescpago;
+            public string ceciclopag;
+            public string cecodemple;
+            public string cenomemple;
+            public string cenomdepto;
+            public string cenomcargo;
+            public string cecorreoel;
+            public decimal incomeTotal;
+            public decimal discountTotal;
+            public decimal total;
+            public List<PayrollDetailRows> detail;
+        }
+
         // GET: Ajax
         public ActionResult Index()
         {
             return View();
+        }
+
+        private string GetCycleFor(int month, int cycle)
+        {
+            if (cycle == 1)
+                return ((month * 2) - 1).ToString();
+
+            return (month * cycle).ToString();
+        }
+
+        public string GetPayrollTemplate(string by, string entityId, string month, string year, string cycle)
+        {
+            string payrollTemplate = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Templates/");
+            payrollTemplate = System.IO.Path.Combine(payrollTemplate, "payrollDetail.html");
+            string content = System.IO.File.ReadAllText(payrollTemplate);
+
+            string _cycle = year + month + GetCycleFor(int.Parse(month), int.Parse(cycle));
+
+            DataSet employees = GetEmployeesPayroll(by, entityId, _cycle);
+            
+            foreach(DataRow row in employees.Tables[0].Rows)
+            {
+                string employeeId_ = row.ItemArray[0].ToString();
+                string cycle_ = row.ItemArray[1].ToString();
+                string paytype_ = row.ItemArray[2].ToString();
+
+                DataSet payroll = GetPayrollDetailForEmployee(employeeId_, cycle_, paytype_);
+
+                PayrollDetailHeader payrollDetail = GetPayrollDetail(payroll);
+
+                content = content.Replace("##cenomdepto##", payrollDetail.cenomdepto);
+                content = content.Replace("##ceciclopag##", payrollDetail.ceciclopag);
+
+                string body = string.Empty;
+
+                foreach(var item in payrollDetail.detail)
+                {
+                  body += "<tr> <td style = 'width: 60%'>" + item.ceingdeduc + "-" +  item.cetipotrans + " " + item.cedesctran + "</td>" +
+                          "<td style = 'width: 20%; text-align: right'>" +
+                          item.ceingdeduc == "I" ? "<span>" + @String.Format("{0:#,##0.00}", item.cevaltrans) + "</span> </td>" : "<td style = 'width: 20%; text-align: right'>" +
+                          "<span>" + @String.Format("{0:#,##0.00}", item.cevaltrans) + "</span> </td>" +
+                          "</tr>";            
+                }
+            }
+            
+            return content;
+        }
+
+        public static DataSet GetPayrollsBy(string employeeId)
+        {
+            string sQuery = "SELECT DISTINCT CECICLOPAG, CEDESCPAGO, CETIPOPAGO from [QS36F.RCNOCE00] WHERE CECODEMPLE = " + employeeId + " ORDER BY 1 DESC";
+
+            if (ConfigurationManager.AppSettings["EnvironmentVolante"] == "PROD")
+                sQuery = sQuery.Replace("[", "").Replace("]", "");
+
+            return ExecuteDataSetODBC(sQuery, null);
+        }
+
+        private static DataSet GetEmployeesPayroll(string by, string entityId, string cycle)
+        {
+            string sQuery = string.Empty;
+
+            if (by == "all")
+            {
+                sQuery = "SELECT DISTINCT CECODEMPLE, CETIPOPAGO, CECICLOPAG " +
+                " FROM [QS36F.RCNOCE00] WHERE CECICLOPAG = '" + cycle + "' ";
+            }
+            else if (by == "directorate")
+            {
+                sQuery = "SELECT DISTINCT CECODEMPLE, CETIPOPAGO, CECICLOPAG " +
+                " FROM [QS36F.RCNOCE00] WHERE CENOMDEPTO = " + entityId +
+                " AND CECICLOPAG = '" + cycle + "'";
+            }
+            else
+            {
+                sQuery = "SELECT DISTINCT CECODEMPLE, CETIPOPAGO, CECICLOPAG " +
+                " FROM [QS36F.RCNOCE00] WHERE CECODEMPLE = " + entityId +
+                " AND CECICLOPAG = '" + cycle + "' GROUP BY CECODEMPLE, CETIPOPAGO, CECICLOPAG";
+            }
+
+            if (ConfigurationManager.AppSettings["EnvironmentVolante"] == "PROD")
+                sQuery = sQuery.Replace("[", "").Replace("]", "");
+
+            return ExecuteDataSetODBC(sQuery, null);
+        }
+
+        public static DataSet GetPayrollDetailForEmployee(string employeeId, string cycle, string paytype)
+        {
+            string sQuery = string.Empty;
+
+            sQuery = "SELECT CECODIRE, CETIPOPAGO, CEDESCPAGO, CEINGDEDUC, CETIPTRANS, CEDESCTRAN, CEVALTRANS, CECICLOPAG," +
+            " CECODEMPLE, CENOMEMPLE, CENOMDEPTO, CENOMCARGO, CECORREOEL FROM [QS36F.RCNOCE00] WHERE CECODEMPLE = " + employeeId +
+            " AND CECICLOPAG = '" + cycle + "' AND CETIPOPAGO = '" + paytype + "' ORDER BY CEINGDEDUC DESC";
+
+
+            if (ConfigurationManager.AppSettings["EnvironmentVolante"] == "PROD")
+                sQuery = sQuery.Replace("[", "").Replace("]", "");
+
+            return ExecuteDataSetODBC(sQuery, null);
+        }
+
+        //Map to object to get all detail for a specific payroll
+        public static PayrollDetailHeader GetPayrollDetail(DataSet data)
+        {
+            PayrollDetailHeader payrollDetail = new PayrollDetailHeader();
+
+            if (data.Tables.Count > 0 && data.Tables[0].Rows.Count > 0)
+            {
+                decimal amount = 0;
+
+                payrollDetail.discountTotal = 0;
+                payrollDetail.incomeTotal = 0;
+                payrollDetail.total = 0;
+
+                payrollDetail.cecodire = data.Tables[0].Rows[0].ItemArray[0].ToString();
+                payrollDetail.cetipopago = data.Tables[0].Rows[0].ItemArray[1].ToString();
+                payrollDetail.cedescpago = data.Tables[0].Rows[0].ItemArray[2].ToString();
+                payrollDetail.ceciclopag = data.Tables[0].Rows[0].ItemArray[7].ToString();
+                payrollDetail.cecodemple = data.Tables[0].Rows[0].ItemArray[8].ToString();
+                payrollDetail.cenomemple = data.Tables[0].Rows[0].ItemArray[9].ToString();
+                payrollDetail.cenomdepto = data.Tables[0].Rows[0].ItemArray[10].ToString();
+                payrollDetail.cenomcargo = data.Tables[0].Rows[0].ItemArray[11].ToString();
+                payrollDetail.cecorreoel = data.Tables[0].Rows[0].ItemArray[12].ToString();
+
+                payrollDetail.detail = new List<PayrollDetailRows>();
+
+                foreach (DataRow row in data.Tables[0].Rows)
+                {
+                    amount = decimal.Parse(row.ItemArray[6].ToString());
+
+                    if (row.ItemArray[3].ToString() == "I") payrollDetail.incomeTotal += amount;
+                    if (row.ItemArray[3].ToString() != "I") payrollDetail.discountTotal += amount;
+
+                    payrollDetail.detail.Add(new PayrollDetailRows
+                    {
+                        ceingdeduc = row.ItemArray[3].ToString(),
+                        cetipotrans = row.ItemArray[4].ToString(),
+                        cedesctran = row.ItemArray[5].ToString(),
+                        cevaltrans = amount
+                    });
+                }
+
+                payrollDetail.total = payrollDetail.incomeTotal - payrollDetail.discountTotal;
+            }
+            return payrollDetail;
         }
 
         public static string SHA256(string randomString)
--- Controllers/PayrollController.cs
+++ Controllers/PayrollController.cs
@@ -6,47 +6,12 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using VolanteNominaRC.Models;
+using static VolanteNominaRC.Controllers.AjaxController;
 
 namespace VolanteNominaRC.Controllers
 {
     public class PayrollController : Controller
     {
-
-        public struct Payroll
-        {
-            public string period;
-            public string year;
-            public string month;
-            public string fortnight;
-            public string description;
-            public string paytype;
-        }
-
-        public struct PayrollDetailRows
-        {
-            public string ceingdeduc;
-            public string cetipotrans;
-            public string cedesctran;
-            public decimal cevaltrans;
-        }
-
-        public struct PayrollDetailHeader
-        {
-            public string cecodire;
-            public string cetipopago;
-            public string cedescpago;
-            public string ceciclopag;
-            public string cecodemple;
-            public string cenomemple;
-            public string cenomdepto;
-            public string cenomcargo;
-            public string cecorreoel;
-            public decimal incomeTotal;
-            public decimal discountTotal;
-            public decimal total;
-            public List<PayrollDetailRows> detail;
-        }
-
         // GET: Payroll
         public ActionResult Index()
         {
@@ -72,14 +37,7 @@ namespace VolanteNominaRC.Controllers
             if (Session["role"] == null) return RedirectToAction("Login", "User");
 
             string EmployeeId = Session["employeeId"].ToString();
-            string sQuery = "SELECT CECODIRE, CETIPOPAGO, CEDESCPAGO, CEINGDEDUC, CETIPTRANS, CEDESCTRAN, CEVALTRANS, CECICLOPAG," +
-                " CECODEMPLE, CENOMEMPLE, CENOMDEPTO, CENOMCARGO, CECORREOEL FROM [QS36F.RCNOCE00] WHERE CECODEMPLE = " + EmployeeId + 
-                " AND CECICLOPAG = '" + period + "' AND CETIPOPAGO = '" + paytype + "' ORDER BY CEINGDEDUC DESC";
-
-            if (ConfigurationManager.AppSettings["EnvironmentVolante"] == "PROD")
-                sQuery = sQuery.Replace("[", "").Replace("]", "");
-
-            DataSet payrollDetail = AjaxController.ExecuteDataSetODBC(sQuery, null);
+            DataSet payrollDetail = GetPayrollDetailForEmployee(EmployeeId, period, paytype);
 
             PayrollDetailHeader payroll = GetPayrollDetail(payrollDetail);
             ViewBag.PayrollDetail = payroll;
@@ -97,12 +55,7 @@ namespace VolanteNominaRC.Controllers
             try
             {
                 string EmployeeId = Session["employeeId"].ToString();
-                string sQuery = "SELECT DISTINCT CECICLOPAG, CEDESCPAGO, CETIPOPAGO from [QS36F.RCNOCE00] WHERE CECODEMPLE = " + EmployeeId + " ORDER BY 1 DESC";
-
-                if (ConfigurationManager.AppSettings["EnvironmentVolante"] == "PROD")
-                    sQuery = sQuery.Replace("[", "").Replace("]", "");
-
-                DataSet payrolls = AjaxController.ExecuteDataSetODBC(sQuery, null);
+                DataSet payrolls = GetPayrollsBy(EmployeeId);
 
                 ViewBag.Payrolls = GetPayrolls(payrolls);
             }
@@ -138,53 +91,7 @@ namespace VolanteNominaRC.Controllers
             }
             return payrolls;
         }
-
-        //Map to object to get all detail for a specific payroll
-        private PayrollDetailHeader GetPayrollDetail(DataSet data)
-        {
-            PayrollDetailHeader payrollDetail = new PayrollDetailHeader();
-
-            if (data.Tables.Count > 0 && data.Tables[0].Rows.Count > 0)
-            {
-                decimal amount = 0;
-
-                payrollDetail.discountTotal = 0;
-                payrollDetail.incomeTotal = 0;
-                payrollDetail.total = 0;
-
-                payrollDetail.cecodire = data.Tables[0].Rows[0].ItemArray[0].ToString();
-                payrollDetail.cetipopago = data.Tables[0].Rows[0].ItemArray[1].ToString();
-                payrollDetail.cedescpago = data.Tables[0].Rows[0].ItemArray[2].ToString();
-                payrollDetail.ceciclopag = data.Tables[0].Rows[0].ItemArray[7].ToString();
-                payrollDetail.cecodemple = data.Tables[0].Rows[0].ItemArray[8].ToString();
-                payrollDetail.cenomemple = data.Tables[0].Rows[0].ItemArray[9].ToString();
-                payrollDetail.cenomdepto = data.Tables[0].Rows[0].ItemArray[10].ToString();
-                payrollDetail.cenomcargo = data.Tables[0].Rows[0].ItemArray[11].ToString();
-                payrollDetail.cecorreoel = data.Tables[0].Rows[0].ItemArray[12].ToString();
-                
-                payrollDetail.detail = new List<PayrollDetailRows>();
-
-                foreach (DataRow row in data.Tables[0].Rows)
-                {
-                    amount = decimal.Parse(row.ItemArray[6].ToString());
-
-                    if (row.ItemArray[3].ToString() == "I") payrollDetail.incomeTotal += amount;
-                    if (row.ItemArray[3].ToString() != "I") payrollDetail.discountTotal += amount;
-                    
-                    payrollDetail.detail.Add(new PayrollDetailRows
-                    {
-                        ceingdeduc = row.ItemArray[3].ToString(),
-                        cetipotrans = row.ItemArray[4].ToString(),
-                        cedesctran = row.ItemArray[5].ToString(),
-                        cevaltrans = amount
-                    });
-                }
-
-                payrollDetail.total = payrollDetail.incomeTotal - payrollDetail.discountTotal;
-            }
-            return payrollDetail;
-        }
-
+        
         //Save when payroll is seen by someone
         private void SavePayrollSeenHistory(string employeeId, string payrollCycle, string seenBy)
         {
